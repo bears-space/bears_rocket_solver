c005039dadd40cab2124815825ab7b54aece02e3
Andrii Kozytskyi <kozytskyi.andrii.d@gmail.com>
2026-09-01 20:34:56 +0200

    Better injector component comments

--- modules/BEARS_Rocket/comp_injector.py
+++ modules/BEARS_Rocket/comp_injector.py
@@ -7,13 +7,16 @@ from openmdao.api import ExplicitComponent
 class InjectorComponent(ExplicitComponent):
 
 	def initialize(self):
+		# Liquid oxidizer density
 		self.options.declare("rho_ox", default=1200.0, types=float)
 
 	def setup(self):
 		self.add_input("p_tank",    val=30e5,   units="Pa")
 		self.add_input("p_chamber", val=20e5,   units="Pa")
 		self.add_input("a_inj",     val=4.5e-5, units="m**2")
-		self.add_input("cd",        val=0.7) # Discharge coefficient (efficiency)
+
+		# Discharge coefficient (efficiency) TODO
+		self.add_input("cd", val=0.7)
 
 		self.add_output("mdot_ox", val=1.0, units="kg/s")
 
@@ -23,12 +26,18 @@ class InjectorComponent(ExplicitComponent):
 	def compute(self, inputs, outputs):
 		p_up   = inputs["p_tank"]
 		p_down = inputs["p_chamber"]
-		rho    = self.options["rho_ox"]
 		area   = inputs["a_inj"]
 		cd     = inputs["cd"]
 
-		dp = p_up - p_down
-		if dp >= 0:
-			outputs["mdot_ox"] = cd * area * np.sqrt(2.0 * rho * dp)
+		rho = self.options["rho_ox"]
+
+		# Upstream (tank) to downstream (fuel chamber) pressure difference
+		delta_p = p_up - p_down
+
+		# Account for possible negative pressure difference (reverse flow)
+		if delta_p >= 0:
+			mdot =  cd * area * np.sqrt(2.0 * rho * delta_p)
 		else:
-			outputs["mdot_ox"] = -cd * area * np.sqrt(2.0 * rho * (-dp))
+			mdot = -cd * area * np.sqrt(2.0 * rho * (-delta_p))
+
+		outputs["mdot_ox"] = mdot
